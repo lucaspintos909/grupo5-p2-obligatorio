d3a0067b24f9cb21e9247b8ac9592c71145eaf98
lucaspintos909 <lucaspintos909@gmail.com>
2023-06-18 19:38:58 -0300

    Agregando usuario al Tweet

--- src/entities/Tweet.java
+++ src/entities/Tweet.java
@@ -6,13 +6,15 @@ public class Tweet {
     private final String source;
     private final boolean isRetweet;
     private final String date;
+    private User user;
 
-    public Tweet(long id, String content, String source, boolean isRetweet, String date) {
+    public Tweet(long id, String content, String source, boolean isRetweet, String date, User user) {
         this.id = id;
         this.content = content.toLowerCase();
         this.source = source;
         this.isRetweet = isRetweet;
         this.date = date;
+        this.user = user;
     }
 
     public long getId() {
@@ -33,5 +35,9 @@ public class Tweet {
 
     public String getDate() {
         return date;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
